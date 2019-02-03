aecd05596cbab0d6911c18385f782ec2ca49da79
Zheng Yang <b8047712@newcastle.ac.uk>
2019-02-03 23:39:42 +0000

    111

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Camera cam = Camera.main;
-        
-	}
+    public Transform player;
+    public float rotateSpeed;
+    Vector3 distanceVec;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+   
+    // Use this for initialization
+    void Start () {
+
+
+        distanceVec = transform.position - player.transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
+
+    void LateUpdate() //对摄像机的操作写在LateUpdate里
+    {
+        Quaternion camTurnAngleX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotateSpeed, Vector3.up);
+        Quaternion camTurnAngleY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * rotateSpeed, Vector3.left);
+
+        distanceVec = camTurnAngleX*camTurnAngleY * distanceVec;
+
+
+        Vector3 newPos = player.transform.position + distanceVec;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+        transform.LookAt(player.transform);
+
+    }
+        
+    
+
 }
